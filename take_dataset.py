a239f9db4ab557f8b181f1eb802fdf50306fc3dd
evanfebrianto <van.evanfebrianto@gmail.com>
2021-07-08 12:30:22 +0800

    added key to change state

--- take_dataset.py
+++ take_dataset.py
@@ -1,19 +1,35 @@
 import cv2
+from os import path, makedirs
 
 WIDTH, HEIGHT = 640, 360
+OUTPUT_DIR = 'webcam_dataset'
+class_names = ['incorrect_mask', 'mask', 'no_mask']
 
 cap = cv2.VideoCapture(2)
 cap.set(3, WIDTH)
 cap.set(4, HEIGHT)
 
+for class_name in class_names:
+    if not path.exists(path.join(OUTPUT_DIR,class_name)):
+        makedirs(path.join(OUTPUT_DIR,class_name))
+
+state = 0
 while cap.isOpened():
     success, image = cap.read()
     if not success:
         print("Ignoring empty camera frame.")
         # If loading a video, use 'break' instead of 'continue'.
         continue
+    print(class_names[state])
     cv2.imshow('Frame', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-            break
+    k = cv2.waitKey(33)
+    if k == ord('q'):
+        break
+    elif k == ord('1'):
+        state = 0
+    elif k == ord('2'):
+        state = 1
+    elif k == ord('3'):
+        state = 2
 cap.release()
 cv2.destroyAllWindows() 
